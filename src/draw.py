f07fe92559ae7a18d227b46681e103768438baef
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-06-22 13:05:51 -0500

    Graph points update

--- src/draw.py
+++ src/draw.py
@@ -11,12 +11,12 @@ graph_data = Graph()
 graph_data.debug_create_test_data()
 print(graph_data.vertexes)
 
-N = 10
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-debug_palette = Spectral8
-debug_palette.append('#ff0000')
-debug_palette.append('#0000ff')
+color_list = []
+for vertex in graph_data.vertexes:
+    color_list.append(vertex.color)
 
 plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
@@ -24,7 +24,7 @@ plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(debug_palette, 'color')
+graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Oval(height=20, width=20, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
@@ -33,9 +33,8 @@ graph.edge_renderer.data_source.data = dict(
 
 # start of layout code
 # Looks like this setting the positions of the vertexes
-circ = [i*2*math.pi/N for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
